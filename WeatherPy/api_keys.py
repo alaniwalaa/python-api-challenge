34d248c549258ab97a6dddd5a6f91215f74b230d
walaa <w.alani2010@yahoo.com>
2021-01-11 00:53:01 -0800

    plot

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5a058f86bf6b7ef5fc26e46a57640cf5"
+#api_key = '079865046ab7b6428c49379638a3b41a'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBYA1bhXKClvUW60WusVOFF-xWyfSjNMTY"
