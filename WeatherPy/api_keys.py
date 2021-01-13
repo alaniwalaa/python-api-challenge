6fa3130c4ae7a42f659f4db7b0e64f394aafad18
walaa <w.alani2010@yahoo.com>
2021-01-12 20:41:59 -0800

    heat map

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
 weather_api_key = "5a058f86bf6b7ef5fc26e46a57640cf5"
-#api_key = '079865046ab7b6428c49379638a3b41a'
 
 # Google API Key
 g_key = "AIzaSyBYA1bhXKClvUW60WusVOFF-xWyfSjNMTY"
